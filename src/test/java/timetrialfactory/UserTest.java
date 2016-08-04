e5912d0a65f6ebf5421814111d71e10d36ccadd5
williamsalerno <williamsalerno@teste-PC.contprog.local>
2016-08-04 09:04:21 -0300

    Signed-off-by: williamsalerno <williamsalerno@teste-PC.contprog.local>

---
+++ src/test/java/timetrialfactory/UserTest.java
@@ -0,0 +1,14 @@
+package timetrialfactory;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Ignore;
+
+public class UserTest {
+
+	@Ignore
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
